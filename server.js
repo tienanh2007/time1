1baf62cb7ad1673c10b5c1f4f575557cdb46b0d2
Tien Anh Nguyen <nguyentienanh68@gmail.com>
2016-06-01 15:00:48 +0000

    2

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(8080, function () {
+var port = process.env.PORT || 8080;
+app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
